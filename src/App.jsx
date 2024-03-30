b1dfb5b6705a3f763b0996b25f2041c5cf2303b0
Thaila Lemes <48499244+thailalemes@users.noreply.github.com>
2024-03-30 20:21:01 -0300

    Feature/part 02 (#2)
    * Sidebar adjustment
    
    * Profile adjustment
    
    * Sidebar and Post implementation
    
    * Style adjustments
    
    * Adjustments on layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,24 @@
-import { Header } from './components/Header';
-import { Post } from './Post'
-
-import './global.css';
+import { Header } from "./components/Header";
+import { Post } from "./components/Post";
+import styles from "./App.module.css";
+import "./global.css";
+import { Sidebar } from "./components/Sidebar";
 
 export function App() {
   return (
     <div>
       <Header />
 
-      <Post
-        author="Thaila Lemes"
-        content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime itaque quas corporis beatae veritatis, reprehenderit asperiores vitae quod possimus qui dignissimos unde deleniti consequatur quae, repellat debitis sunt, est rerum!"
-      />
-      <Post
-        author="Thaila Telles"
-        content="Um novo post muito legal"
-      />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="Thaila Lemes"
+            content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime itaque quas corporis beatae veritatis, reprehenderit asperiores vitae quod possimus qui dignissimos unde deleniti consequatur quae, repellat debitis sunt, est rerum!"
+          />
+          <Post author="Thaila Telles" content="Um novo post muito legal" />
+        </main>
+      </div>
     </div>
-  )
+  );
 }
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,28 @@
+import styles from "./Sidebar.module.css";
+import { PencilLine } from "@phosphor-icons/react";
+
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img 
+      className={styles.cover}
+      src="https://images.unsplash.com/photo-1522252234503-e356532cafd5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=50" 
+      alt="" 
+      />
+
+      <div className={styles.profile}>
+        <img className={styles.avatar} src="https://github.com/thailalemes.png" alt="" />
+
+        <strong>Thaila Lemes</strong>
+        <span>Web Developer</span>
+      </div>
+
+      <footer>
+        <a href="#">
+            <PencilLine size={20} />
+            Editar seu perfil
+        </a>
+      </footer>
+    </aside>
+  );
+}
