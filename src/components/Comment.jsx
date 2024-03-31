06e66cb13b3f23f71ef72d07689c7c22ac530b65
Thaila Lemes <thailalemes@Thailas-MacBook-Air.local>
2024-03-31 02:05:46 -0300

    Including new comments in page and adjusting css

---
+++ src/components/Comment.jsx
@@ -0,0 +1,38 @@
+import styles from './Comment.module.css';
+
+import { ThumbsUp, Trash } from '@phosphor-icons/react'
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img
+        className={styles.avatar}
+        src="https://github.com/thailalemes.png"
+        alt=""
+      />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Thaila Lemes</strong>
+              <time title="31 de Marco as 01:08" dateTime="2024-03-31 01:08:30">
+                Cerca de 1h atras
+              </time>
+            </div>
+
+            <button title="Deletar comentario">
+                <Trash size={24} />
+            </button>
+          </header>
+
+          <p>Muito bom Devon, parabens!</p>
+        </div>
+        <footer>
+            <ThumbsUp />
+            Aplaudir <span>20</span>
+        </footer>
+      </div>
+    </div>
+  );
+}
